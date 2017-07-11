b98e7b98d6db8a69123aa6a3bd747cc491fc4803
zoebisch <zoebisch@gmail.com>
2017-07-10 21:37:33 -0700

    Moved search methods into Concerns::Searchable and include them in PriceManager Class.  Created Concerns::Statistical module and moved find_stats into module

--- lib/concerns/concerns.rb
+++ lib/concerns/concerns.rb
@@ -1,14 +1,42 @@
 
 module Concerns
 
-  def compile_by_name
-    # list = self.all.sort{|a,b| a.name <=> b.name}.uniq
-    # Item.all.each do |item|
-    #   if item[:title].include?(CL_Scraper.item)
-    #     puts "found #{CL_Scraper.item} in item, #{item.listing}"
-    #   end
-    # end
-    # list.each_with_index{|obj,ind| yield(obj,ind)}
+  module Searchable
+
+    def search_by_type(search_item)
+      results = []
+      @list.each{|item| results << item if item[:title].include?(search_item)}
+      results
+    end
+
+    def sort_by_price(list)
+      value_items = []
+      list.each{|item| value_items << item if item[:price] != nil}
+      value_items.sort{|a,b| a[:price] <=> b[:price]}
+    end
+
+    def compile_by_name
+      # list = self.all.sort{|a,b| a.name <=> b.name}.uniq
+      # Item.all.each do |item|
+      #   if item[:title].include?(CL_Scraper.item)
+      #     puts "found #{CL_Scraper.item} in item, #{item.listing}"
+      #   end
+      # end
+      # list.each_with_index{|obj,ind| yield(obj,ind)}
+    end
+
+  end
+
+  module Statistical
+    attr_accessor :volume, :mean
+    def find_stats(list)
+      sum = 0
+      values = []
+      list.each{|item| sum += item[:price]}
+      list.each{|item| values << item[:price]}
+      @volume = list.size
+      @mean = sum/@volume
+    end
   end
 
 end
--- lib/price_manager.rb
+++ lib/price_manager.rb
@@ -1,42 +1,23 @@
 require 'pry'
-#require_relative './concerns/concerns'
+require_relative './concerns/concerns.rb'
 
 require_relative './craigslist_scraper.rb'
 require_relative './item.rb'
 class PriceManager
-  attr_accessor :mean, :volume
   @list = []
-  #extend Concerns::Searchable
+  include Concerns::Searchable
+  include Concerns::Statistical
   #MENU = []
 
   def call
     url = "https://seattle.craigslist.org"
     item_array = CL_Scraper.new(url,"furniture").scrape_category
     @list = Item.create_from_collection(item_array)
-    results = search_by_type("desk")
+    results = search_by_type("bed")
     blah = sort_by_price(results)
     find_stats(blah) if blah != []
     binding.pry
   end
-
-  def search_by_type(search_item)
-    results = []
-    @list.each{|item| results << item if item[:title].include?(search_item)}
-    binding.pry
-    results
-  end
-
-  def sort_by_price(list)
-    value_items = []
-    list.each{|item| value_items << item if item[:price] != nil}
-    value_items.sort{|a,b| a[:price] <=> b[:price]}
-  end
-
-  def find_stats(list)
-    values = 0
-    list.each{|item| values += item[:price]}
-    @volume = list.size
-    @mean = values/@volume
-  end
+  
 end
 PriceManager.new.call
