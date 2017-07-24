c91e70de30719561a3051cf61256589e9fce9aa7
zoebisch <zoebisch@gmail.com>
2017-07-24 14:06:45 -0700

    moved around require, require_relative to function

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@ require "nokogiri"
 require "open-uri"
 
 require_relative '../lib/concerns/concerns.rb'
+require_relative "../lib/craigslist_price_it_right/version"
 require_relative '../lib/craigslist_scraper.rb'
 require_relative '../lib/price_manager.rb'
 require_relative '../lib/item.rb'
