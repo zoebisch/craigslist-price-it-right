5c74b5997a046312cebcabd1336ae14302a7f104
zoebisch <zoebisch@gmail.com>
2017-07-24 15:48:15 -0700

    updated file name for main rb file, moved ../lib/craigslist_price_it_right to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,9 @@
-require "nokogiri"
-require "open-uri"
+# require "nokogiri"
+# require "open-uri"
 
 require_relative '../lib/concerns/concerns.rb'
-require_relative "../lib/craigslist_price_it_right/version"
+require_relative '../lib/craigslist_price_it_right/version'
+require_relative '../lib/craigslist_price_it_right'
 require_relative '../lib/craigslist_scraper.rb'
 require_relative '../lib/price_manager.rb'
 require_relative '../lib/item.rb'
